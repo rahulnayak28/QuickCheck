2c42ae06e1c31421026164aa785c96e81c3bbe90
rahulnayak28 <rahulnayak28@gmail.com>
2023-09-23 13:53:53 +0530

    Added Transformer T5 for summarizing the text.

---
+++ QuickCheck.py
@@ -0,0 +1,31 @@
+import streamlit as st
+import torch
+from transformers import AutoTokenizer, AutoModelWithLMHead
+
+tokenizer = AutoTokenizer.from_pretrained('t5-base')
+model = AutoModelWithLMHead.from_pretrained('t5-base', return_dict=True)
+
+
+# Streamlit App
+def main():
+    st.title("Quick Text Summarization app")
+    text = st.text_area("Enter Text Here")
+
+    if st.button("Summarize"):
+        if text:
+            st.subheader("Summary")
+            # initialize the tokenizer model:
+            inputs = tokenizer.encode("summarize: " + text, return_tensors='pt', max_length=512, truncation=True)
+
+            # generate the summary by using the model.generate function on T5:
+            summary_ids = model.generate(inputs, max_length=150, min_length=80, length_penalty=5., num_beams=2)
+
+            # decode the tokenized summary using the tokenizer.decode function:
+            summary = tokenizer.decode(summary_ids[0])
+            st.write(summary)
+        else:
+            st.warning("Please enter some text to summarize.")
+
+
+if __name__ == "__main__":
+    main()
